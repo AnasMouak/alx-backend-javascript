dd58f48706e29bf0e84e82f52229958adc192a85
AnasMouak <anasmouak1111@gmail.com>
2024-07-25 12:29:20 +0000

    A Course, Getters, and Setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,34 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+  get name() {
+    return this._name;
+  }
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._name = value;
+  }
+  get length() {
+    return this._length;
+  }
+  set length(value) {
+    if (!Number.isInteger(value)) {
+      throw new Error('Length must be a number');
+    }
+    this._length = value;
+  }
+  get students() {
+    return this._students;
+  }
+  set students(value) {
+    if (!Array.isArray(value) || !value.every(item => typeof item === 'string')) {
+      throw new Error('Students must be an array of strings');
+    }
+    this._students = value;
+  }
+}
