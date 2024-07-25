debdc301a51ca4f7b7ea7219cfb0a05809936781
AnasMouak <anasmouak1111@gmail.com>
2024-07-25 13:05:49 +0000

    Implement a method

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,25 +3,30 @@ export default class Currency {
     this._code = code;
     this._name = name;
   }
+
   get code() {
     return this._code;
   }
+
   set code(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Code must be a string');
     }
     this._code = value;
   }
+
   get name() {
     return this._name;
   }
+
   set name(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a number');
     }
     this._name = value;
   }
+
   displayFullCurrency() {
-    return`${this.name} (${this.code})`;
+    return `${this.name} (${this.code})`;
   }
 }
