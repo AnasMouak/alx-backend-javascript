4a2cfef3b087935cee0c70422efb5e210f8d8013
AnasMouak <anasmouak1111@gmail.com>
2024-09-19 18:14:21 +0000

    Spies

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,17 @@
+const Utils = {
+  calculateNumber: function (type, a, b) {
+    if (type === 'SUM') {
+      return Math.round(a) + Math.round(b);
+    } else if (type === 'SUBTRACT') {
+      return Math.round(a) - Math.round(b);
+    } else if (type === 'DIVIDE') {
+      if (Math.round(b) === 0) {
+        return 'Error';
+      }
+      return Math.round(a) / Math.round(b);
+    }
+    throw new Error('Invalid type');
+  }
+};
+
+module.exports = Utils;
